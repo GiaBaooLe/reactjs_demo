65517c36ad16d9c81f567b57af10dc44979aa0df
GiabaooLe <baolngse171848@fpt.edu.vn>
2023-09-27 09:07:46 +0700

    Components Part 1

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <p>copyright © 2022</p>
+        </div>
+    );
+};
+
+export default Footer;
